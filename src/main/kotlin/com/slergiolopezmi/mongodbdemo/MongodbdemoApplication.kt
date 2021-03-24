400480a676e5e11aa079f476cd74e9dbb71db0bb
Sergio López <lopezmillansergio@gmail.com>
2021-03-24 19:20:36 +0100

    Initial commit

---
+++ src/main/kotlin/com/slergiolopezmi/mongodbdemo/MongodbdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.slergiolopezmi.mongodbdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongodbdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MongodbdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/slergiolopezmi/mongodbdemo/MongodbdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slergiolopezmi.mongodbdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongodbdemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
